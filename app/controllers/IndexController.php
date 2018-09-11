62a5e31513877697a95f16a3d83d0a16081be862
lmichelletech <lmichelle@techlaunch.io>
2018-09-11 17:51:31 -0400

    index page design

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -4,6 +4,31 @@ use Phalcon\Mvc\Controller;
 
 class IndexController extends Controller
 {
-    public function indexAction() {
+    public function initialize()
+    {
+        // get the current page
+        $this->view->page = basename($_SERVER['REQUEST_URI']);
+        
     }
+
+    public function indexAction() 
+    {
+        
+    }
+
+    
+
+    public function searchSubmitAction() {
+        // get data from the URL
+        $search = $this->request->get('search');
+
+
+        // search database
+        
+
+        // redirect to the index 
+        
+    }
+
+   
 }
