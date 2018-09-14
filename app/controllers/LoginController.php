02fb9bf16aff9307970963ecf75e10e25c9fad94
lmichelletech <lmichelle@techlaunch.io>
2018-09-13 21:38:41 -0400

    Created Signup Page

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -2,10 +2,12 @@
 
 use Phalcon\Mvc\Controller;
 
+
 class LoginController extends Controller
 {
     public function indexAction() 
     {
+        $this->view->title = "Login";
         $error = $this->request->get('error');
         $this->view->error = $error;
     }
@@ -15,10 +17,10 @@ class LoginController extends Controller
         // get data from the URL
         $email = $this->request->get('email');
         $password = md5($this->request->get('password'));
-
+        
         // connect to the databas and pull the user
         $user = User::findFirst("email='$email' AND password='$password'");
-
+        
         // check if data is ok
         if($user) {
             // create the session
@@ -36,6 +38,49 @@ class LoginController extends Controller
         }
     }
 
+    public function signupSubmitAction() 
+    {
+        $this->view->disable();
+
+        if($this->request->isPost()) {
+            $dataSent = $this->request->getPost();
+
+            $auser = new User();
+            $auser->name = $dataSent["name"];
+            $auser->email = $dataSent["email"];
+            $auser->password = md5($dataSent["password"]);
+            $auser->picture = $dataSent["picture"];
+
+            if(!$auser->picture){
+                $auser->picture = "http://1.bp.blogspot.com/-Kudj45DvQSk/VlfWVCpeGMI/AAAAAAAACbw/qbDKhTo0TNQ/s000/no_image.jpg";
+            }
+
+            $savedSuccessfully = $auser->save();
+
+            if($savedSuccessfully) {
+                // create the session
+                $obj = new stdClass();
+                $obj->name = $auser->name;
+                $obj->email = $auser->email;
+                $obj->picture = $auser->picture;
+                
+                $this->session->set('user', $obj);
+                
+                // redirect to the admin
+                $this->response->redirect('/admin');
+            } else {
+                $messages = $auser->getMessages();
+
+                echo "Sorry, the following problems were generated: ";
+                foreach ($messages as $message) {
+                    echo "$message <br/>";
+                }
+            }
+        } else {
+            echo "The request method should be POST!";  
+        }
+    }
+
     public function logoutAction()
     {
         // close session
@@ -43,5 +88,10 @@ class LoginController extends Controller
 
         // redirect to login page
         $this->response->redirect('/login');
+    }
+
+    public function signupAction() 
+    {
+        $this->view->title = "Sign Up";
     }
 }
